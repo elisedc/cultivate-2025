99e9d7fa3947808d4cf11ee037eda86a9b88ed23
Elise Cheng <dscheng96@gmail.com>
2025-07-24 12:58:23 +0800

    add welcome page animations

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Cultivate 2025" },
+    { name: "description", content: "Welcome to Cultivate 2025!" },
   ];
 }
 

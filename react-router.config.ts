3fcfbfdba6ba4e12805cae28468ca6d90a07018b
Elise Cheng <dscheng96@gmail.com>
2025-07-24 15:24:13 +0800

    configure routes for gh pages and add README

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  ssr: false, // Github Pages does not support SSR
+  ssr: false, // GitHub Pages does not support SSR
   basename: '/cultivate-2025', // Set the base path for the application
 } satisfies Config;
